079605577c9abd2c0f9fb0e7308e5dc8f56270e2
Gabor <orbangabeszb@gmail.com>
2019-12-17 15:30:17 +0100

    socket moved to try-with-resources

--- src/com/orgabor/UDPServerApp.java
+++ src/com/orgabor/UDPServerApp.java
@@ -8,9 +8,8 @@ import java.net.SocketException;
 public class UDPServerApp {
 
 	public static void main(String[] args) {
-		try {
-			DatagramSocket socket = new DatagramSocket(5000);
-			
+		try(DatagramSocket socket = new DatagramSocket(5000)) {
+
 			while(true) {
 				byte[] buffer = new byte[50];
 				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
