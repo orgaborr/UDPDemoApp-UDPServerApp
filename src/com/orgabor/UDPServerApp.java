5955882377f8a27a4243ecf08f87068fa3c623c5
Gabor <orbangabeszb@gmail.com>
2019-12-22 12:03:01 +0100

    packet sent back to client

--- src/com/orgabor/UDPServerApp.java
+++ src/com/orgabor/UDPServerApp.java
@@ -3,6 +3,7 @@ package com.orgabor;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class UDPServerApp {
@@ -15,6 +16,13 @@ public class UDPServerApp {
 				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 				socket.receive(packet);
 				System.out.println("Text received is: " + new String(buffer, 0, packet.getLength()));
+				
+				String returnString = "echo: " + new String(buffer, 0, packet.getLength());
+				byte[] buffer2 = returnString.getBytes();
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				packet = new DatagramPacket(buffer2, buffer2.length, address, port);
+				socket.send(packet);
 			}
 			
 		} catch(SocketException e) {
